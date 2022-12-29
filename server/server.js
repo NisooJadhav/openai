68411a473270c58a32592d9c7e98a3f2c5b19c96
nisoojadhav <nisoojadhav@gmail.com>
2022-12-29 13:54:52 +0530

    init

---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "hello from OpenAI",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0.5,
+      max_tokens: 2500,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ e });
+  }
+});
+
+app.listen(5000, () =>
+  console.log("server running on port http://localhost:5000")
+);
